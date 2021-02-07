a3eb377bd9453d6f26000baf6ed7b405012ffe21
wanggangx <wanggangx@luxin.cn>
2021-02-07 17:07:00 +0800

    first commit

---
+++ src/main/java/IsdpApplication.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsdpApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(IsdpApplication.class, args);
+	}
+}
---
+++ src/main/java/org/isdp/se/config/SmartEngineProperties.java
@@ -0,0 +1,25 @@
+package org.isdp.se.config;
+
+public class SmartEngineProperties {
+}
+
+//
+//	@Override
+//	public void setUp() {
+//		super.setUp();
+//
+//		BasicServiceTaskDelegation.resetCounter();
+//		ExclusiveTaskDelegation.resetCounter();
+//	}
+//
+//	@Test
+//	public void test() throws Exception {
+//
+//		ExecutionInstance executionInstance = commonCodeSnippet("basic-process.bpmn.xml");
+//
+//		Map<String, Object> request = new HashMap<String, Object>();
+//		request.put("route", "a");
+//
+//		commonCode(request, executionInstance);
+//
+//	}
